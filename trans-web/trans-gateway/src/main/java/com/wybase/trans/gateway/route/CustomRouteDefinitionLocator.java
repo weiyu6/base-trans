a91115ad1f8bbe72b967b03b6c07787ee6b1bc90
weiyu <weiyulearn@163.com>
2024-04-11 18:10:27 +0800

    修改网关中加载路由方式

--- trans-web/trans-gateway/src/main/java/com/wybase/trans/gateway/route/CustomRouteDefinitionLocator.java
+++ trans-web/trans-gateway/src/main/java/com/wybase/trans/gateway/route/CustomRouteDefinitionLocator.java
@@ -1,30 +1,30 @@
-package com.wybase.trans.gateway.route;
-
-import jakarta.annotation.Resource;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
-
-/**
- * 自定义路由定义，用于从外部源加载路由规则
- * @author weiyu
- * @date 2024/4/9
- */
-@Component
-public class CustomRouteDefinitionLocator implements RouteDefinitionLocator {
-    @Resource
-    private ExternalConfigurationSource externalConfigurationSource;
-
-    /**
-     * 获取路由定义
-     * @return
-     */
-    @Override
-    public Flux<RouteDefinition> getRouteDefinitions() {
-        List<RouteDefinition> definitions = externalConfigurationSource.loadRouteDefinitions();
-        return Flux.fromIterable(definitions);
-    }
-}
+//package com.wybase.trans.gateway.route;
+//
+//import jakarta.annotation.Resource;
+//import org.springframework.cloud.gateway.route.RouteDefinition;
+//import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+//import org.springframework.stereotype.Component;
+//import reactor.core.publisher.Flux;
+//
+//import java.util.List;
+//
+///**
+// * 自定义路由定义，用于从外部源加载路由规则
+// * @author weiyu
+// * @date 2024/4/9
+// */
+//@Component
+//public class CustomRouteDefinitionLocator implements RouteDefinitionLocator {
+//    @Resource
+//    private ExternalConfigurationSource externalConfigurationSource;
+//
+//    /**
+//     * 获取路由定义
+//     * @return
+//     */
+//    @Override
+//    public Flux<RouteDefinition> getRouteDefinitions() {
+//        List<RouteDefinition> definitions = externalConfigurationSource.loadRouteDefinitions();
+//        return Flux.fromIterable(definitions);
+//    }
+//}
---
+++ trans-web/trans-gateway/src/main/java/com/wybase/trans/gateway/route/GatewayServiceHandler.java
@@ -0,0 +1,61 @@
+package com.wybase.trans.gateway.route;
+
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * 核心配置类，项目初始化加载数据库的路由配置
+ *
+ * @author weiyu
+ * @date 2024/4/11
+ */
+@Component
+public class GatewayServiceHandler implements ApplicationEventPublisherAware, CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(GatewayServiceHandler.class);
+
+    @Resource
+    private RouteDefinitionWriter routeDefinitionWriter;
+    @Resource
+    private ExternalConfigurationSource externalConfigurationSource;
+    private ApplicationEventPublisher publisher;
+
+    /**
+     * 项目启动时加载路由配置
+     */
+    @Override
+    public void run(String... args) {
+        loadRouteConfig();
+    }
+
+    /**
+     * 加载路由配置
+     */
+    private void loadRouteConfig() {
+        // 从外部配置源加载路由定义
+        List<RouteDefinition> definitions = externalConfigurationSource.loadRouteDefinitions();
+        logger.info("网关配置信息：=====>{}", definitions);
+        // 逐个保存路由定义到路由定义写入器中
+        definitions.forEach(definition -> routeDefinitionWriter.save(Mono.just(definition)).subscribe());
+        // 发布路由刷新事件
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+    }
+
+    /**
+     * 获取路由配置
+     */
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
+}
